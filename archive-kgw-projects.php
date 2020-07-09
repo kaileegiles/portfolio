e9da9b7227cfcc9efaa474cee860dfa891cfc3e1
kgiles <info@kgiles.com>
2020-07-09 14:10:27 -0700

    Update archive-kgw-projects.php

--- archive-kgw-projects.php
+++ archive-kgw-projects.php
@@ -21,7 +21,7 @@ get_header();
 					</header><!-- .page-header -->
 					<p>This page is regularly updated as I’m always learning, refining, and adding more projects.</p>
 					<p class="current-project-title"><strong>Currently working on:</strong></p>
-					<p class="current-project">An expense tracker built in React. Final result coming soon.</p>
+					<p class="current-project">A productivity tracker built in React. Final result coming soon.</p>
 				</div> <!--end text-container -->
 				<div class="line-01"></div>
 				<div class="line-02"></div>
@@ -30,35 +30,52 @@ get_header();
 			<div class="project-content">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="project-tile">
-					<header class="entry-header">
-						<?php
-						if ( is_singular() ) :
-							the_title( '<h2 class="entry-title">', '</h2>' );
-						else :
-							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-						endif; ?>
+				$args = array (
+					'post_type' => 'kgw-projects',
+					'posts_per_page' => -1,
+					'post_status' => 'publish',
+					'orderby' => 'publish_date',
+					'order' => 'DESC'
+				);
 
-					</header><!-- .entry-header -->
+				$projects_query = new WP_Query( $args );
 
-					<?php if ( function_exists ( 'get_field' ) ) {
-						?> <p><?php the_field( 'project_tools' );?></p>
-						<div class="wp-block-buttons">
-							<div class="wp-block-button is-style-outline dark">
-								<a class="wp-block-button__link" href="<?php echo esc_url(get_permalink() ) ?>">See process</a>
-							</div><!-- end outline -->
-						</div><!-- end block-buttons -->
-					<?php } ?>
-					</div> <!-- end project-tile -->
+				if ( $projects_query->have_posts()) {
 
-				</article><!-- #post-<?php the_ID(); ?> -->
+					/* Start the Loop */
+					while ( $projects_query->have_posts() ) :
+						$projects_query->the_post(); ?>
 
-			<?php endwhile;
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="project-tile">
+								<header class="entry-header">
+									<?php
+									if ( is_singular() ) :
+										the_title( '<h2 class="entry-title">', '</h2>' );
+									else :
+										the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+									endif; ?>
+
+								</header><!-- .entry-header -->
+
+								<?php if ( function_exists ( 'get_field' ) ) {
+									?> <p><?php the_field( 'project_tools' );?></p>
+									<div class="wp-block-buttons">
+										<div class="wp-block-button is-style-outline dark">
+											<a class="wp-block-button__link" href="<?php echo esc_url(get_permalink() ) ?>">See process</a>
+										</div><!-- end outline -->
+									</div><!-- end block-buttons -->
+								<?php } ?>
+								</div> <!-- end project-tile -->
+
+							</article><!-- #post-<?php the_ID(); ?> -->
+
+					<?php endwhile;
+
+				}
+
+				wp_reset_postdata();
 
 			the_posts_navigation();
 
